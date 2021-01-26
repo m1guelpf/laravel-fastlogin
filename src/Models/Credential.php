7aa1bd00ee49a9d79af1dc07cbede49f87ef2ad3
chrisrenga <13778529+chrisrenga@users.noreply.github.com>
2021-01-26 13:51:55 +0000

    Add fillable fields to Credential Model
    fixes #2

--- src/Models/Credential.php
+++ src/Models/Credential.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Credential extends Model
 {
     use HasFactory;
+	
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+		'credId',
+		'key',
+	];
 }
